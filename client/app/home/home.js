0bdb2b1f301ce98e84fcfb4acb03835250727a73
Alex Leonetti <leonetti.tech@gmail.com>
2016-02-05 15:35:20 -0800

    Add Added pagination and some styling - need bugs to be fixed

--- client/app/home/home.js
+++ client/app/home/home.js
@@ -4,21 +4,49 @@ angular.module('myAngular.home', [])
   console.log('Initialized Home Controller');
 
   $scope.items = [];
+  $scope.currentPage = 1;
+  $scope.perPage = 15;
+  $scope.numberOfPages = 1;
+  $scope.pages = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34];
 
   var request = function(text) {
     $http({
         method: 'GET',
-        url: 'http://api.vip.supplyhub.com:19000/products?search=' + text + "&limit=150"
+        url: 'http://api.vip.supplyhub.com:19000/products?search=' + text + "&limit=500"
       }).then(function success(response) {
-        $scope.items = response.data;
+        if(response.data["0"].product) {
+          $scope.items = response.data;
+          $scope.numberOfPages = Math.ceil($scope.items.length/$scope.perPage);
+        } else {
+          $scope.items = []
+          $scope.numberOfPages = 0;
+        }
       }, function error(response) {
-        console.log(response);
+        console.log('error');
       })
     };
 
   $scope.search = function(text) {
+    console.log($scope.currentPage);
     if(text) {
       $timeout(request(text), 100);
     }
+  }
+
+  $scope.changePage = function(page) {
+    $scope.currentPage = page;
+  }
+
+  $scope.pageColor = function(page) {
+    if(page === $scope.currentPage) {
+      return 'red';
+    }
+  }
+})
+
+.filter('startFrom', function(){
+  return function(input, start) {
+    start = +start;
+    return input.slice(start);
   }
 });
